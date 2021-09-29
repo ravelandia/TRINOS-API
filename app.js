0bd3b758e02ed3be23e25ed66a05f0665ce24434
Rafael Velandia <ravelandia@uninorte.edu.co>
2021-09-29 22:49:26 +0000

    Fix: Se arreglaron todos todos los errores

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const indexRouter = require('./src/routes/index');
+const usersRouter = require('./src/routes/users');
 
 const app = express();
 
@@ -12,7 +11,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
